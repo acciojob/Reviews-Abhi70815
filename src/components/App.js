61d9cb972b06d33c2566f9ad08d1b08dea03852b
Abhi70815 <abhishekchurasia45621@gmail.com>
2024-08-15 09:40:02 +0530

    save

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ import Review from './Review'
 const App = () => {
   return (
    <main>
-    <section className="container" id='review-heading'>
-     
+    <section className="container">
+     <title id="review-heading">Our Reviews</title>
       <Review/>
     </section>
    </main>
